0d16eab01979bda77ea54b7cfce5b12a0a67c4f2
Silvio <silviolima@gmail.com>
2020-02-24 17:30:52 -0300

    Face detect

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 mkdir -p ~/.streamlit/
+
+
 echo "\
 [server]\n\
 headless = true\n\
@@ -6,3 +8,8 @@ port = $PORT\n\
 enableCORS = false\n\
 \n\
 " > ~/.streamlit/config.toml
+
+echo "\
+[general]\n\
+email = \"silcesarlima@yahoo.com.br\"\n\
+" > ~/.streamlit/credentials.toml
