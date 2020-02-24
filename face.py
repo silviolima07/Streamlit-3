8fd5a844158251eb647fd5f997e1f7780472a53b
Silvio <silviolima@gmail.com>
2020-02-24 17:52:31 -0300

    Face detect

--- face.py
+++ face.py
@@ -1,10 +1,12 @@
 # Core Pkgs
 import streamlit as st 
 import cv2
+'''
 from PIL import Image,ImageEnhance
 import numpy as np 
 import os
-'''
+
+
 face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_eye.xml')
 smile_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_smile.xml')
