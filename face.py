5455575bd3f1b4459183b2b59c7922fb851337be
Silvio <silviolima@gmail.com>
2020-02-24 20:18:29 -0300

    Face detect

--- face.py
+++ face.py
@@ -1,6 +1,12 @@
 # Core Pkgs
 import streamlit as st 
 
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_smile.xml')
+
 
 def main():
 	"""Face Detection App"""
