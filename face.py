31f6de5efdfd7a234d58967f2ba70c2b2f7ea71b
Silvio <silviolima@gmail.com>
2020-02-24 20:01:13 -0300

    Face detect

--- face.py
+++ face.py
@@ -18,30 +18,6 @@ def main():
 	st.title("Face Detection App")
 	st.text("Build with Streamlit and OpenCV")
 
-	activities = ["Detection","About"]
-	choice = st.sidebar.selectbox("Select Activty",activities)
-
-	if choice == 'Detection':
-		st.subheader("Face Detection")
-
-		image_file = st.file_uploader("Upload Image",type=['jpg','png','jpeg'])
-
-		if image_file is not None:
-			our_image = Image.open(image_file)
-			st.text("Original Image")
-			# st.write(type(our_image))
-			st.image(our_image)
-
-
-		# Face Detection
-		task = ["Faces","Smiles","Eyes","Cannize","Cartonize"]
-		feature_choice = st.sidebar.selectbox("Find Features",task)
-		if st.button("Process"):
-
-			if feature_choice == 'Faces':
-				result_img,result_faces = detect_faces(our_image)
-				st.image(result_img)
-
 
 if __name__ == '__main__':
 	main()	
