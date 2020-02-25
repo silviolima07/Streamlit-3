88ad315d39147e2e7315cbd0bcca1ef11c017ef8
Silvio <silviolima@gmail.com>
2020-02-25 01:08:46 -0300

    Face Detect

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 #web:  sh setup.sh && streamlit run --server.enableCORS false --server.port $PORT face.py
-web:  sh setup.sh && streamlit run face.py
+web:  sh setup.sh && streamlit run app.py
 
--- app.py
+++ app.py
@@ -2,8 +2,7 @@
 import streamlit as st 
 from PIL import Image,ImageEnhance
 import numpy as np 
-import os
-import cv2 # sem isso abriu a pagina ok
+import os, cv2
 
 def main():
     """ A simple iris EDA App """
@@ -13,3 +12,16 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+# path  
+path = r'/home/silvio/github/Streamlit-3/pessoas.jpeg'
+  
+# Reading an image in default mode 
+image = cv2.imread(path) 
+  
+# Window name in which image is displayed 
+window_name = 'image'
+  
+# Using cv2.imshow() method  
+# Displaying the image  
+cv2.imshow(window_name, image)
