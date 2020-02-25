8045a91af1aea195727d93173960fdd35d6cdcf8
Silvio <silviolima@gmail.com>
2020-02-25 01:23:53 -0300

    Face detect

--- app.py
+++ app.py
@@ -14,14 +14,14 @@ if __name__ == '__main__':
     main()
 
 # path  
-path = r'/home/silvio/github/Streamlit-3/pessoas.jpeg'
+#path = r'/home/silvio/github/Streamlit-3/pessoas.jpeg'
   
 # Reading an image in default mode 
-image = cv2.imread(path) 
+#image = cv2.imread(path) 
   
 # Window name in which image is displayed 
-window_name = 'image'
+#window_name = 'image'
   
 # Using cv2.imshow() method  
 # Displaying the image  
-cv2.imshow(window_name, image)
+#cv2.imshow(window_name, image)
