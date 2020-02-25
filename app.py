a50701a61b20d23b350264258019df951a605cca
Silvio <silviolima@gmail.com>
2020-02-25 02:05:04 -0300

    Face detect

--- app.py
+++ app.py
@@ -8,11 +8,7 @@
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# This demo lets you to explore the Udacity self-driving car image dataset.
-# More info: https://github.com/streamlit/demo-self-driving
+# See the License for the specific language governing permissions 
 
 import streamlit as st
 import altair as alt
